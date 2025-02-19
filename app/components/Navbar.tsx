401a396b4291f109176060fab3d9919083bb050d
DeKingoye <yannmouandza3@gmail.com>
2025-02-19 15:18:11 +0100

    testoyann

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                     {/* <Layers className='h-6 w-6'/> */}
                 </div>
                 <span className='ml-3 font-bold text-2xwl italic'>
-                Yann <span className='text-accent'>Agency testNiniYoyo</span>
+                Yann <span className='text-accent'>Agency testNiniYoyo2</span>
                 </span>
             </div>
             <div className='flex space-x-4 items-center'>
